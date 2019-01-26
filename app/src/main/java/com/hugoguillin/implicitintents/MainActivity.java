b13a9b84fe05ab76b931e2d9b6aeba57951742ec
Hugo Guillín <hugoguillin@hotmail.com>
2019-01-26 18:44:18 +0100

    Task_1

---
+++ app/src/main/java/com/hugoguillin/implicitintents/MainActivity.java
@@ -0,0 +1,23 @@
+package com.hugoguillin.implicitintents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openWebsite(View view) {
+    }
+
+    public void openLocation(View view) {
+    }
+
+    public void shareText(View view) {
+    }
+}
